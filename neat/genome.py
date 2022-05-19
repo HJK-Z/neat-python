286f58d05e6b856b4b796a6eada9e5f776c00d8a
Justin Ki <hjk.businessemail@gmail.com>
2022-05-19 01:45:11 -0700

    Update genome.py

--- neat/genome.py
+++ neat/genome.py
@@ -582,7 +582,7 @@ class DefaultGenome(object):
         Create a partially connected genome only connected to the last output,
         including (possibly) direct input-output connections.
         """
-        output_id = config.output_keys[len(config.output_keys)-1]
+        output_id = len(config.output_keys)-1
         input = [i for i in self.nodes if i in config.input_keys]
         for input_id in input:
             connection = self.create_connection(config, input_id, output_id)
